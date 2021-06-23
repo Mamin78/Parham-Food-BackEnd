d148737439a60995d1c76c1b1a674cb44abbaea2
Mamin78 <aminsh78@gmail.com>
2021-06-23 17:36:30 +0430

    delete comments

--- router/router.go
+++ router/router.go
@@ -6,34 +6,6 @@ import (
 	"github.com/labstack/gommon/log"
 )
 
-//func New() *echo.Echo {
-//	e := echo.New()
-//	e.Logger.SetLevel(log.DEBUG)
-//
-//	e.Logger.SetLevel(log.ERROR)
-//	e.Use(middleware.Logger())
-//	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
-//		SigningKey: []byte(handler.Key),
-//		Skipper: func(c echo.Context) bool {
-//			// Skip authentication for signup and login requests
-//			if c.Path() == "/login" || c.Path() == "/signup" || c.Path() == "/restaurant" {
-//				return true
-//			}
-//			return false
-//		},
-//	}))
-//
-//	e.Pre(middleware.RemoveTrailingSlash())
-//	e.Use(middleware.Logger())
-//	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-//		AllowOrigins: []string{"*"},
-//		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
-//		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-//	}))
-//	e.Validator = NewValidator()
-//	return e
-//}
-
 func New() *echo.Echo {
 	e := echo.New()
 	e.Logger.SetLevel(log.DEBUG)
